1f958fb95568f9e1645c017b95bf112165c20efa
Florian Delezenne <del.florian@gmail.com>
2012-03-11 15:44:09 +0100

    Test

--- phpic/resources/config.php
+++ phpic/resources/config.php
@@ -16,10 +16,9 @@ or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/library'));
 defined("TEMPLATES_PATH")
 or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/templates'));
 
+
 /*
  Error reporting.
 */
 ini_set("error_reporting", "true");
 error_reporting(E_ALL|E_STRCT);
-
-?>
