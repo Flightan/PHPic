d2c87047e5a5265a8ae79d8b1cdc61da49d07203
Florian Delezenne <del.florian@gmail.com>
2012-03-17 14:38:52 +0100

    Passage a la librairie zend

--- phpic/resources/templates/footer.php
+++
@@ -1,6 +0,0 @@
-<hr>
-<footer>
-	<p>&copy; PHPic 2012</p>
-</footer>
-</body>
-</html>
--- phpic/resources/templates/home.php
+++
@@ -1,7 +0,0 @@
-<div id="home" class="content">
-	<div class="page-header">
-		<h1>Home</h1>
-		
-		<a href="login.php">Login</a>
-	</div>
-</div>
