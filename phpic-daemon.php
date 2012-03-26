3da86ad63639a1571fc1116f6a973c70d193453f
Florian Delezenne <del.florian@gmail.com>
2012-03-26 17:22:46 +0200

    Debug daemon

--- phpic-daemon.php
+++ phpic-daemon.php
@@ -1,10 +1,6 @@
 <?php
 
-//Recherche des utilisateurs
-//$path_users = realpath(APPLICATION_PATH . '/../public/users');
-$path_users = realpath('./public/users/');
-
-function getUserList ($dir)
+function getUserList ($dir, $path_users)
 {
 	$users = array();
 
@@ -28,8 +24,10 @@ function prettySave ($simpleXML, $path)
 	$dom->save($path);
 }
 
-function parseAlbumXML ($xml_album_path)
+function parseAlbumXML ($xml_album_path, $dir_user, $user_path)
 {
+	$exists_albums = array();
+
 	$xml_album = simplexml_load_file($xml_album_path);
 	foreach($xml_album->children() as $k=>$item)
 	{
@@ -87,180 +85,182 @@ function parseAlbumXML ($xml_album_path)
 		{
 			$xml_photo_path = $album_path . "/photo.xml";
 			//Test l'existance du photo.xml de l'album
-			if (file_exists($xml_photo_path))
+			if (!file_exists($xml_photo_path))
 			{
-				$xml_photo = simplexml_load_file($xml_photo_path);
-				$exists_photo = array();
-				//Parcours du xml pour recuperer les photo éxistante
-				foreach($xml_photo->children() as $k=>$item)
-				{
-					if($k == "picture")
-					{
-						$exists_photo[] = $item['url'][0];
-					}
-				}
-				//Array des chemins pour la création des miniatures
-				$thumbnail_paths = array();
-				$suppr_photo_path = array();
-				//Recuperation des chemin des photos
-				while (false !== ($photo_rep = readdir($dir_album)))
-				{
-					if ($photo_rep != "." && $photo_rep != "..")
-					{
-						if (is_dir($album_path . "/" . $photo_rep))
-						{
-							continue;
-						} else {
-							if ($photo_rep != "photo.xml") {
-								$suppr_photo_path[] = $photo_rep;
-								if (in_array($photo_rep, $exists_photo))
-								{
-									//La photo éxiste déjà
-									continue;
-								} else {
-									//add photo dans la liste des chemins pour les miniature
-									$new_photo = $xml_photo->addChild('picture');
-									$new_photo->addAttribute('url', $photo_rep);
-									prettySave($xml_photo, $xml_photo_path);
-									$thumbnail_paths[] = $album_path . "/" . $photo_rep;
-								}
-							}
-						}
-					}
-				}
-				//Comparaison des fichier et du photo.xml pour la suppression
-				foreach ($exists_photo as $suppr)
-				{
-					if (!in_array($suppr, $suppr_photo_path))
-					{
-						foreach($xml_photo->children() as $k=>$item)
-						{
-							if(($k == "picture") && ($item['url'][0] == $suppr))
-							{
-								$node = $xml_photo->xpath("/album/picture[@url='" . $suppr ."']");
-								$suppNode = dom_import_simplexml($node[0]);
-								$suppNode->parentNode->removeChild($suppNode);
-								prettySave($xml_photo, $xml_photo_path);
-							}
-						}
-					}
-				}
-				//Call miniature fonction param = $thumbnail_paths
-					
-				foreach ($thumbnail_paths as $path)
-				{
-					// Je retrouve le nom du fichier avec l'extension
-					$filename = basename($path);
-					$info = pathinfo($filename);
-
-					// Je prend le nom du fichier sans l'extension qui ell est peut etre trouvee d'apres $info['extension']
-					//$filename =  basename($filename,'.'.$info['extension']);
-
-					// J'essaye d'avoir tout le chemin jusqu'au dernier repertoire c-a-d sans le nom du fichier
-					$dirname = dirname($path);
-
-					// Je cree mon objet source qui contient l'image originale
-					switch (strtolower($info['extension']))
-					{
-						case 'jpg':
-						case 'jpeg':
-							$source = imagecreatefromjpeg($path);
-							break;
-						case 'png':
-							$source = imagecreatefrompng($path);
-							break;
-						case 'gif':
-							$source = imagecreatefromgif($path);
-							break;
-						default:
-							$source = imagecreatefromjpeg($path);
-					}
-
-					// Je prend les dimensions de l'image
-					list($width, $height) = getimagesize($path);
-
-					if ($width > $height)
-					{
-						$width_full = 800;
-						$height_full = 600;
-						$width_thumb = 220;
-						$height_thumb = 165;
-						$cropWidth_thumb = 220;
-						$cropHeight_thumb = 165;
-					}
-					else
-					{
-						$width_full = 600;
-						$height_full = 800;
-						$width_thumb = 250;
-						$height_thumb = 330;
-						$cropWidth_thumb = 220;
-						$cropHeight_thumb = 165;
-					}
-
-					// Creation des images de destination
-					$destination_full = imagecreatetruecolor($width_full, $height_full);
-					$destination_thumb = imagecreatetruecolor($cropWidth_thumb, $cropHeight_thumb);
-
-					// Redimensionnement
-					imagecopyresized($destination_full, $source, 0, 0, 0, 0, $width_full, $height_full, $width, $height);
-					imagecopyresized($destination_thumb, $source, 0, 0, 0, 0, $width_thumb, $height_thumb, $width, $height);
-
-					$dir_for_full = $dirname.'/full';
-					// On verifie l'existance du repertoire qui va contenir les images (full size) - On le cree s'il n'existe pas
-					if (!file_exists($dir_for_full))
-					mkdir($dir_for_full, 0755);
-
-					$dir_for_thumbs = $dirname.'/thumbnails';
-					// On verifie l'existance du repertoire qui va contenir les images (thumbnails) - On le cree s'il n'existe pas
-					if (!file_exists($dir_for_thumbs))
-					mkdir($dir_for_thumbs, 0755);
-
-					// Copie des images sur le disque
-					$path_full = $dir_for_full.'/'.$filename;
-					$path_thumb = $dir_for_thumbs.'/'.$filename;
-
-					switch (strtolower($info['extension']))
-					{
-						case 'jpg':
-						case 'jpeg':
-							imagejpeg($destination_full, $path_full, 100);
-							imagejpeg($destination_thumb, $path_thumb, 100);
-							break;
-						case 'png':
-							imagepng($destination_full, $path_full, 100);
-							imagepng($destination_thumb, $path_thumb, 100);
-							break;
-						case 'gif':
-							imagegif($destination_full, $path_full);
-							imagegif($destination_thumb, $path_thumb);
-							break;
-						default:
-							imagejpeg($destination_full, $path_full, 100);
-						imagejpeg($destination_thumb, $path_thumb, 100);
-					}
-
-					// Destruction des objets temporaires
-					imagedestroy($destination_full);
-					imagedestroy($destination_thumb);
-					imagedestroy($source);
-				}
-
-			} else {
 				//Ajout de l'xml photo si il n'éxiste pas
 				$root_node = "<album></album>";
 				$new_xml = new SimpleXMLElement($root_node);
-				$new_xml->AddAttribute('theme', 'nature');
-				$new_xml->AddAttribute('display', 'compact');
-				prettySave($xml_photo, $xml_photo_path);
+				prettySave($new_xml, $xml_photo_path);
+
+			}
+			$xml_photo = simplexml_load_file($xml_photo_path);
+			$exists_photo = array();
+			//Parcours du xml pour recuperer les photo éxistante
+			foreach($xml_photo->children() as $k=>$item)
+			{
+				if($k == "picture")
+				{
+					$exists_photo[] = $item['url'][0];
+				}
+			}
+			//Array des chemins pour la création des miniatures
+			$thumbnail_paths = array();
+			$suppr_photo_path = array();
+			//Recuperation des chemin des photos
+			while (false !== ($photo_rep = readdir($dir_album)))
+			{
+				if ($photo_rep != "." && $photo_rep != "..")
+				{
+					if (is_dir($album_path . "/" . $photo_rep))
+					{
+						continue;
+					} else {
+						if ($photo_rep != "photo.xml") {
+							$suppr_photo_path[] = $photo_rep;
+							if (in_array($photo_rep, $exists_photo))
+							{
+								//La photo éxiste déjà
+								continue;
+							} else {
+								//add photo dans la liste des chemins pour les miniature
+								$new_photo = $xml_photo->addChild('picture');
+								$new_photo->addAttribute('url', $photo_rep);
+								prettySave($xml_photo, $xml_photo_path);
+								$thumbnail_paths[] = $album_path . "/" . $photo_rep;
+							}
+						}
+					}
+				}
+			}
+			//Comparaison des fichier et du photo.xml pour la suppression
+			foreach ($exists_photo as $suppr)
+			{
+				if (!in_array($suppr, $suppr_photo_path))
+				{
+					foreach($xml_photo->children() as $k=>$item)
+					{
+						if(($k == "picture") && ($item['url'][0] == $suppr))
+						{
+							$node = $xml_photo->xpath("/album/picture[@url='" . $suppr ."']");
+							$suppNode = dom_import_simplexml($node[0]);
+							$suppNode->parentNode->removeChild($suppNode);
+							prettySave($xml_photo, $xml_photo_path);
+						}
+					}
+				}
+			}
+			//Call miniature fonction param = $thumbnail_paths
+
+			foreach ($thumbnail_paths as $path)
+			{
+				// Je retrouve le nom du fichier avec l'extension
+				$filename = basename($path);
+				$info = pathinfo($filename);
+
+				// Je prend le nom du fichier sans l'extension qui ell est peut etre trouvee d'apres $info['extension']
+				//$filename =  basename($filename,'.'.$info['extension']);
+
+				// J'essaye d'avoir tout le chemin jusqu'au dernier repertoire c-a-d sans le nom du fichier
+				$dirname = dirname($path);
+
+				// Je cree mon objet source qui contient l'image originale
+				switch (strtolower($info['extension']))
+				{
+					case 'jpg':
+					case 'jpeg':
+						$source = imagecreatefromjpeg($path);
+						break;
+					case 'png':
+						$source = imagecreatefrompng($path);
+						break;
+					case 'gif':
+						$source = imagecreatefromgif($path);
+						break;
+					default:
+						$source = imagecreatefromjpeg($path);
+				}
+
+				// Je prend les dimensions de l'image
+				list($width, $height) = getimagesize($path);
+
+				if ($width > $height)
+				{
+					$width_full = 800;
+					$height_full = 600;
+					$width_thumb = 220;
+					$height_thumb = 165;
+					$cropWidth_thumb = 220;
+					$cropHeight_thumb = 165;
+				}
+				else
+				{
+					$width_full = 600;
+					$height_full = 800;
+					$width_thumb = 250;
+					$height_thumb = 330;
+					$cropWidth_thumb = 220;
+					$cropHeight_thumb = 165;
+				}
+
+				// Creation des images de destination
+				$destination_full = imagecreatetruecolor($width_full, $height_full);
+				$destination_thumb = imagecreatetruecolor($cropWidth_thumb, $cropHeight_thumb);
+
+				// Redimensionnement
+				imagecopyresized($destination_full, $source, 0, 0, 0, 0, $width_full, $height_full, $width, $height);
+				imagecopyresized($destination_thumb, $source, 0, 0, 0, 0, $width_thumb, $height_thumb, $width, $height);
+
+				$dir_for_full = $dirname.'/full';
+
+				// On verifie l'existance du repertoire qui va contenir les images (full size) - On le cree s'il n'existe pas
+				if (!file_exists($dir_for_full))
+					mkdir($dir_for_full, 0755);
+
+				$dir_for_thumbs = $dirname.'/thumbnails';
+				// On verifie l'existance du repertoire qui va contenir les images (thumbnails) - On le cree s'il n'existe pas
+				if (!file_exists($dir_for_thumbs))
+					mkdir($dir_for_thumbs, 0755);
+
+				// Copie des images sur le disque
+				$path_full = $dir_for_full.'/'.$filename;
+				$path_thumb = $dir_for_thumbs.'/'.$filename;
+
+				switch (strtolower($info['extension']))
+				{
+					case 'jpg':
+					case 'jpeg':
+						imagejpeg($destination_full, $path_full, 100);
+						imagejpeg($destination_thumb, $path_thumb, 100);
+						break;
+					case 'png':
+						imagepng($destination_full, $path_full, 100);
+						imagepng($destination_thumb, $path_thumb, 100);
+						break;
+					case 'gif':
+						imagegif($destination_full, $path_full);
+						imagegif($destination_thumb, $path_thumb);
+						break;
+					default:
+						imagejpeg($destination_full, $path_full, 100);
+					imagejpeg($destination_thumb, $path_thumb, 100);
+				}
+
+				// Destruction des objets temporaires
+				imagedestroy($destination_full);
+				imagedestroy($destination_thumb);
+				imagedestroy($source);
 			}
 		}
 	}
 }
 
+
+$path_users = getcwd() . "/public/users/";
+
 if ($dir = opendir($path_users))
 {
-	$users = getUserList($dir);
+
+	$users = getUserList($dir, $path_users);
 
 	// Parcour des dossier utilisateur
 	foreach($users as $us)
@@ -271,14 +271,14 @@ if ($dir = opendir($path_users))
 			$xml_album_path = $user_path . "/album.xml";
 			$exists_albums = array();
 			//Test si le xml album exist, parcour du xml
-			if (file_exists($xml_album_path))
+			if (!file_exists($xml_album_path))
 			{
-				parseAlbumXML($xml_album_path);
-			} else {
 				$root_node = "<collection></collection>";
 				$new_xml = new SimpleXMLElement($root_node);
-				prettySave($xml_album, $xml_album_path);
+				prettySave($new_xml, $xml_album_path);
 			}
+
+			parseAlbumXML($xml_album_path, $dir_user, $user_path);
 		}
 	}
 }
--- script.sh
+++ script.sh
@@ -3,8 +3,6 @@
 chmod -R 744 ./public
 chmod 777 ./application/logins
 
-path=pwd
-crontab -l > file
-echo '* * * * * php '.$path.'/phpic-daemon.php >/dev/null 2>&1' >> file
-crontab file
-rm file
+path=`pwd`
+
+{ echo "* * * * * php $path/phpic-daemon.php"; } | crontab -
